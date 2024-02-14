5780394673f20589dcdca780edd8709fec70229b
jiesuehong <jshong@zentropy.co.kr>
2024-02-14 16:16:24 +0900

    script 수수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,28 +12,12 @@ echo ""
 echo "------------------------Before Run Script--------------------------------"
 echo "$(ps -ef | grep $SERVICE_NAME | grep java)"
 
-case $1 in
-   reboot)
-      sudo kill -9 $(pgrep -f "sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME")
-      sudo kill -9 $(pgrep -f "${JAVA_SCRIPT} ./$SERVICE_NAME")
-      pushd "${SOURCE_DIR}nzeus/api" > /dev/null
-      echo -ne '\n' | sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME >/dev/null 2>&1 &
-      ;;
+sudo kill -9 $(pgrep -f "sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME")
+sudo kill -9 $(pgrep -f "${JAVA_SCRIPT} ./$SERVICE_NAME")
+pushd "${SOURCE_DIR}nzeus/api" > /dev/null
+echo -ne '\n' | sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME >/dev/null 2>&1 &
+;;
 
-       *)
-        echo ""
-        echo $i
-        echo "Usage:"
-        echo " ./deploy.sh [Command]"
-        echo ""
-        echo "Command:"
-        echo "  start       Start services"
-        echo "  stop        Stop services and remove"
-        echo "  reboot      reboot services"
-        echo ""
-
-        exit 1
-esac
 
 popd > /dev/null
 sleep 0.3
