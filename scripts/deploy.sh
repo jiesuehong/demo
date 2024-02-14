fffc1d5e9bf3678830a92535136c1ce6ecccf2d7
jiesuehong <jshong@zentropy.co.kr>
2024-02-14 17:38:33 +0900

    teswt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,33 +6,35 @@ JAVA_SCRIPT="java -jar"
 SERVICE_NAME="demo-0.0.1-SNAPSHOT.jar"
 DEPLOY_LOG="/home/ubuntu/works/www/demo/deploy.log"
 
-echo "CURRENT DATE"
-echo $(date "+%F %T")
-echo ""
-
-echo "------------------------Before Run Script--------------------------------"
-echo "$(ps -ef | grep $SERVICE_NAME | grep java)" >> $DEPLOY_LOG
+echo "* -------------------------------- START DEPLOY SCRIPT -------------------------------- *"
+echo "|                                                                                       |"
+echo "|                            CURRENT DATE: $(date "+%F %T")                             |"
+echo "|                                                                                       |"
+echo "|                                 Before Run Script                                     |"
+echo "$(ps -ef | grep $SERVICE_NAME | grep java)"
 
 CURRENT_PID="$(pgrep -f 'sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME')"
-ehco "$CURRENT_PID"
 
 if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  echo "|                                 $SERVICE_NAME 종료                                   |"
   sudo kill -9 $(pgrep -f "sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME")
   sudo kill -9 $(pgrep -f "${JAVA_SCRIPT} ./$SERVICE_NAME")
 fi
 
+echo "|                                  $SERVICE_NAME 실행                                    |"
 pushd "${SOURCE_DIR}" > /dev/null
 echo -ne '\n' | sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME >/dev/null 2>&1 &
 popd > /dev/null
 
 sleep 0.3
-echo ""
-echo ""
-echo ""
-echo "------------------------After Run Script---------------------------------"
+echo "|                                                                                       |"
+echo "|                                                                                       |"
+echo "|                                  AFTER RUN SCRIPT                                     |"
 echo "$(ps -ef | grep $SERVICE_NAME | grep java)"
 echo -ne '\n'
 
+echo "|                                                                                       |"
+echo "|                                                                                       |"
+echo "* ------------------------------- FINISH DEPLOY SCRIPT -------------------------------- *"
 
 exit $?
