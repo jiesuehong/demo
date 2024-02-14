4be8189c1a9d4ef9a6b45e702d2d14a4d89a1c13
jiesuehong <jshong@zentropy.co.kr>
2024-02-14 17:17:15 +0900

    추가 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,18 +11,17 @@ echo $(date "+%F %T")
 echo ""
 
 echo "------------------------Before Run Script--------------------------------"
-echo "$(ps -ef | grep $SERVICE_NAME | grep java)"
+echo "$(ps -ef | grep $SERVICE_NAME | grep java)" >> $DEPLOY_LOG
 
-CURRENT_PID="$(ps -ef | grep $SERVICE_NAME | grep java)"
+CURRENT_PID="$(pgrep -f 'sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME')"
 ehco "$CURRENT_PID"
 
 if ![ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -9 $CURRENT_PID
-
-#sudo kill -9 $(pgrep -f "sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME")
-#sudo kill -9 $(pgrep -f "${JAVA_SCRIPT} ./$SERVICE_NAME")
+  sudo kill -9 $(pgrep -f "sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME")
+  sudo kill -9 $(pgrep -f "${JAVA_SCRIPT} ./$SERVICE_NAME")
 fi
+
 pushd "${SOURCE_DIR}" > /dev/null
 echo -ne '\n' | sudo nohup ${JAVA_SCRIPT} ./$SERVICE_NAME >/dev/null 2>&1 &
 popd > /dev/null
